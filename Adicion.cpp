b42b3a11bc8ed24e3c719ade63e1b9f98527bc52
facumarin <ma_facu@hotmail.com>
2016-04-15 08:32:48 -0300

    Added files via upload

---
+++ Adicion.cpp
@@ -0,0 +1,21 @@
+/* 
+Apellido y Nombre: Marin Facundo 
+Curso: K1051 
+Legajo: 158.829-1 
+Fecha: 15/04/2016 
+*/ 
+
+#include <iostream>  
+
+ 
+int main ()
+{
+	int a,b;
+	std::cout << "Ingrese el primer valor: ";
+	std::cin >> a;
+	std::cout << "Ingrese el segundo valor: ";
+	std::cin >> b;
+	std::cout << "El total de la suma es: " << (a+b);
+}
+	
+	
